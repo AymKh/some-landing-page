e65b49ff66193a35cfc79effe858f46e409ea35b
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-06-04 22:51:02 +0100

    tab section quick refactor

--- js/index.js
+++ js/index.js
@@ -1,21 +1,31 @@
 const tablOne = document.querySelector('#tabOneContent');
 const tablTwo = document.querySelector('#tabTwoContent');
+const tabTwoTrigger = document.querySelector('#tabTrigger_2');
+const tabOneTrigger = document.querySelector('#tabTrigger_1');
 
+const showTabOne = _ => {
+    tablTwo.style.display = 'none';
+    tabTwoTrigger.classList.remove('selectedTab');
 
-const toggleTab = tabNumber => {
-    if (tabNumber === 1) {
-        tablTwo.style.display = 'none';
-        tablOne.style.display = 'block';
-    } else {
-        tablTwo.style.display = 'block';
-        tablOne.style.display = 'none';
-    }
+    tablOne.style.display = 'block';
+    tabOneTrigger.classList.add('selectedTab');
 }
 
+const showTabTwo = _ => {
+    tablTwo.style.display = 'block';
+    tabTwoTrigger.classList.add('selectedTab');
 
-var glide = new Glide(".glide", {
+    tablOne.style.display = 'none';
+    tabOneTrigger.classList.remove('selectedTab');
+}
+
+const glide = new Glide(".glide", {
     type: "carousel",
     focusAt: "center",
     perView: 4
 });
+
+
+// ******
 glide.mount();
+showTabOne();
